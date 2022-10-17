169e6cfd5939146ff0a8e9bb167930247ad4808a
The Seeker <110098713+theseeker99@users.noreply.github.com>
2022-10-18 03:06:06 +0530

    Update support.py

--- src/commands/support.py
+++ src/commands/support.py
@@ -6,9 +6,9 @@ def support(message, userLanguage=None):
     userLanguage = userLanguage or dbSql.getSetting(message.chat.id, 'language')
 
     markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton(text=language['joinChannelBtn'][userLanguage], url='t.me/h9youtube'))
-    markup.add(telebot.types.InlineKeyboardButton(text=language['shareWithFriendsBtn'][userLanguage], url=f"https://t.me/share/url?url=t.me/torrenthuntbot&text={language['shareText'][userLanguage]}"), telebot.types.InlineKeyboardButton(text=language['joinDiscussionBtn'][userLanguage], url='https://t.me/+mHmBQ2X3hB4zMDll'))
-    markup.add(telebot.types.InlineKeyboardButton(text=language['subscribeChannelBtn'][userLanguage], url='https://youtube.com/h9youtube'), telebot.types.InlineKeyboardButton(text=language['followGithubBtn'][userLanguage], url='https://github.com/hemantapkh'))
-    markup.add(telebot.types.InlineKeyboardButton(text=language['donateBtn'][userLanguage], url=f"https://buymeacoffee.com/hemantapkh"))
+    markup.add(telebot.types.InlineKeyboardButton(text=language['joinChannelBtn'][userLanguage], url='https://t.me/the_seeker_s_cave'))
+    markup.add(telebot.types.InlineKeyboardButton(text=language['shareWithFriendsBtn'][userLanguage], url=f"https://t.me/share/url?url=t.me/TSSC_Torrent_Robot&text={language['shareText'][userLanguage]}"), telebot.types.InlineKeyboardButton(text=language['joinDiscussionBtn'][userLanguage], url='https://t.me/Master_Torrenz_s_Cave'))
+    markup.add(telebot.types.InlineKeyboardButton(text=language['subscribeChannelBtn'][userLanguage], url='https://www.youtube.com/channel/UCs2B-CslcZ0sTH6Tesp_-Kg'), telebot.types.InlineKeyboardButton(text=language['followGithubBtn'][userLanguage], url='https://theseekerscave.wixsite.com/seekerscave'))
+    markup.add(telebot.types.InlineKeyboardButton(text=language['donateBtn'][userLanguage], url=f"https://www.buymeacoffee.com/theseekerscave"))
     
     bot.send_message(message.chat.id, language['support'][userLanguage].format(language['supportBtn'][userLanguage]), reply_markup=markup, disable_web_page_preview=True)
